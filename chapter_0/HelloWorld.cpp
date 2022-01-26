b09a27643d57d95ee4988ef3c5b8e3ba5a3e36fd
jishnuperiya <jishnuperiya@gmail.com>
2022-01-26 23:28:40 +0100

    exclamation add

--- chapter_0/HelloWorld.cpp
+++ chapter_0/HelloWorld.cpp
@@ -4,7 +4,7 @@
 
 int main(){
     std::string name;
-    std::cout << "Enter your name: "<<std::endl;
+    std::cout << "Enter your name!!: "<<std::endl;
     std::cin >> name;
     std::cout << "welcome,"+name<<std::endl;
     return 0;
